c676ef17bbd55839e35dc4f52b85baaef7eed7f2
krigjo25 <krigjo25@gmail.com>
2022-06-24 16:50:26 +0200

    initial commit

--- Python/Learn Python and learn it well within one day/Notes/Chapter 4 - Data types in python.py
+++ Python/Learn Python and learn it well within one day/Notes/Chapter 4 - Data types in python.py
@@ -9,59 +9,8 @@ Author
 
                                                     Chapter 4 - Data types in python
 
- Basic data types integer, float and string
-advanced data types : list, tuple and dictionary
+ 
 
-4.1 Integers
-    Numbers with out decimal places eg. -5 - 5
-    declaration variableName = initial value
-    f.eks mobile numbers equals integers.
-
-4.2 Float
-    Numbers with decimal parts as 3.14 etc.
-    same declaration as integers
-
-4.3 Strings
-    strings equals text ('', "")
-
-4.4 Type Casting in Python
-    Converting string into integers, integers into string etc
-    bultin function : int(), str(),float()
-    int("1") converts into 1, but int("1.2") doesnt work neither does int("str")
-
-4.5 List
-    Refers to a collection of dataq which are normally related. They are ordered
-    e.g 
-        uAlias = ['jhon', 'Jake', 'Timmb']
-        uAge = [21,23,24,51,53]
-
-    Can be accsessed using index 
-    e.g 
-    uAlias[0]
-
-    Notation uAlias[start:end] is known as slice, includes a third number known as stepper uAlias[start:end:Stepper]
-    Slices has a default value whihch is zero
-
-    bult-in functions :
-        .append() - Add one more to the list,
-        del list[index] - remove one from the list.
-
-4.6 Tuples
-    Declaring a tuple : tupleName = ()
-    ex :
-        monthsOfYear = ("Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec")
-
-    Same as list, beside it can NOT be altered when the tuple is set.
-
-4.7 Dictonary
-        Dictionary : dictionaryName {key:data} / dict(key:data)
-                                        - keys are requered to accsess the information, keys can be strings, integers
-
-    A collection of related data PAIRS
-    Set same as dicitonary besides the key, and they're not ordered.
-    acsessing a dictonary dictName[key]
-    adding values to a dictonary dictName['key'] = value
-    removing values from a dictonary del dictName[key]
 
 '''
 
