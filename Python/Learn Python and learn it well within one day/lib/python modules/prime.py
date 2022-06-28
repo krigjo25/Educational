d9ed00df930dba65b214c3bce612106a3c6372d1
krigjo25 <krigjo25@gmail.com>
2022-06-28 23:50:36 +0200

    initial commit

--- Python/Learn Python and learn it well within one day/lib/python modules/prime.py
+++
@@ -1,7 +0,0 @@
-# functions
-# To check if the number is odd or even
-def checkIfPrime (numberToCheck):
-    for x in range(2,numberToCheck): # Using the variable to check if the number canbe devided by two
-        if(numberToCheck%x == 0):
-            return False
-    return True
